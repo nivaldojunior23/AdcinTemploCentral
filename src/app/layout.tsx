2b0d2da7d43319022ab5423aebe8443428405bc4
Nivaldo Junior <nivaldojunior117@gmail.com>
2026-05-29 19:09:14 -0300

    feat: implement security headers, SEO metadata, and RootLayout configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -67,6 +67,9 @@ export default function RootLayout({
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
           precedence="default"
         />
         <script
